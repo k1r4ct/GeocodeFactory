c1d65790d930cc5849b642a57946ddd014fab582
k1r4ct <sfokakashi@gmail.com>
2025-03-19 10:02:46 +0100

    [RELEASE]0.4

--- GeocodeFactory5/administrator/com_geofactory/controllers/markerset.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('_JEXEC') or die;
-
-use Joomla\CMS\MVC\Controller\FormController;
-
-class GeofactoryControllerMarkerset extends FormController
-{
-    protected $text_prefix = 'COM_GEOFACTORY';
-}
--- GeocodeFactory5/administrator/com_geofactory/sql/updates/mysql/5.14.0918.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `#__geofactory_markersets` 	ADD COLUMN `language` VARCHAR(255) NOT NULL DEFAULT '*';
-ALTER TABLE `#__geofactory_ggmaps` 		ADD COLUMN `language` VARCHAR(255) NOT NULL DEFAULT '*';
